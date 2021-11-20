04b7a9399ded8151df1923f3fce9c6fd02bc07df
choeyeongbi <cyeongb@gmail.com>
2021-11-20 16:26:22 +0900

    practice 2 생성

--- 1/index.js
+++ 1/index.js
@@ -1,1 +1,21 @@
+// 변수는 camelCase
 let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+  console.log(count);
+}
+
+//DOM
+//Document : html 문서
+// Object :  html 에 표현되어있는 elements
+// model : javascript로 표현하는 document.
+// --> how use javscript to modify a website
+
+function save() {
+  console.log(count);
+}
+
+save();
