a7115a9d2acc0753a9f58746b675cb71a9e20ab9
choeyeongbi <cyeongb@gmail.com>
2021-11-22 23:25:41 +0900

    counter 작업 구현 javascript

--- 1/index.js
+++ 1/index.js
@@ -3,7 +3,7 @@ let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
-  count = count + 1;
+  count += 1;
   countEl.innerText = count;
   console.log(count);
 }
---
+++ 2/index.js
@@ -0,0 +1,9 @@
+"use-strict";
+let welcomEl = document.getElementById("welcome-el");
+
+let name = "gambi ";
+let greeting = " i love ";
+
+welcomEl.innerText = greeting + name;
+
+welcomEl.innerText += " ❤";
