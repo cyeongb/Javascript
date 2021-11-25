ca7d14a2ccf0032f405bb26ed5564e343b5c950f
choeyeongbi <cyeongb@gmail.com>
2021-11-25 17:55:57 +0900

    간단한 계산기 만들기 complete

--- 2/index.js
+++ 2/index.js
@@ -1,1 +1,33 @@
 "use-strict";
+
+let num1 = 8;
+let num2 = 2;
+
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+let sumEl = document.getElementById("sum-el");
+
+function add() {
+  let result = num1 + num2;
+  sumEl.textContent = "값: " + result;
+  console.log(sumEl);
+}
+
+function substract() {
+  let result = num1 - num2;
+  sumEl.textContent = "값: " + result;
+  console.log(sumEl);
+}
+
+function divide() {
+  let result = num1 / num2;
+  sumEl.textContent = "값: " + result;
+  console.log(sumEl);
+}
+
+function multiply() {
+  let result = num1 * num2;
+  sumEl.textContent = "값: " + result;
+  console.log(sumEl);
+}
