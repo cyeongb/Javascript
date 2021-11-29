2296573ad4349fbbf1e06820ccacdd3f6caeae0a
choeyeongbi <cyeongb@gmail.com>
2021-11-29 21:46:21 +0900

    add blackjack

--- blackjack/index.js
+++ blackjack/index.js
@@ -11,9 +11,13 @@ let isAlive = true;
 //3. create a variable sum, and set it to the sum of two card
 let message = "";
 let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el"); //querySelector가 더 동적입니다.
+
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  sumEl.textContent = "Sum: " + sum;
   if (sum < 21) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
