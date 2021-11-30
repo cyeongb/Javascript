734fc07a0885086d3198f7e922e4eaa5708554ad
choeyeongbi <cyeongb@gmail.com>
2021-11-30 20:00:20 +0900

    complete blackjack

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,22 +1,26 @@
 "use-strict";
 
-// 1. create two variables firstcard and secondcard
+let firstCard = 5;
+let secondCard = 7;
 
-let firstCard = 10;
-let secondCard = 9;
+let cards = [firstCard, secondCard, thirdCard, fourthCard, fifthCard]; //ordered 된 리스트
+
 let sum = firstCard + secondCard;
-// 2. set their values to a random number between 2~11
 let hasBlackJack = false;
 let isAlive = true;
-//3. create a variable sum, and set it to the sum of two card
 let message = "";
 let messageEl = document.getElementById("message-el");
 // let sumEl = document.getElementById("sum-el");
 let sumEl = document.querySelector("#sum-el"); //querySelector가 더 동적입니다.
-
 let cardsEl = document.getElementById("cards-el");
 
+// create a startGame() that calls renderGame()
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1];
   sumEl.textContent = "Sum: " + sum;
   if (sum < 21) {
     message = "Do you want to draw a new card?";
@@ -29,4 +33,12 @@ function startGame() {
   }
   sumEl.textContent = "Sum:" + sum;
   messageEl.textContent = message;
+}
+
+function newCard() {
+  //1. create a card variable as a hard coded number
+  let newCard = 3;
+  sum += newCard;
+  renderGame();
+  //2.
 }
