dbec2258b423a03eb721f3301c53979a537a22b4
choeyeongbi <cyeongb@gmail.com>
2021-12-02 12:09:37 +0900

    가위바위보 예제

--- prac/index.js
+++ prac/index.js
@@ -1,2 +1,20 @@
-let randomNum = Math.floor(Math.random() * 14 + 1);
-console.log(randomNum);
+let hands = ["rock", "scissor", "paper"];
+
+let i = Math.floor(Math.random() * 3);
+let j = Math.floor(Math.random() * 3);
+
+function getHand() {
+  let hand1 = hands[i];
+  let hand2 = hands[j];
+  console.log(hand1, hand2);
+
+  if (
+    (hand1 === "rock" && hand2 === "rock") ||
+    (hand1 === "paper" && hand2 === "paper") ||
+    (hand1 === "scissor" && hand2 === "scissor")
+  ) {
+    console.log("비김!");
+  }
+}
+
+getHand();
