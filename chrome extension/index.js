effbfe8c773bcd682221e61350665e19eb705acd
choeyeongbi <cyeongb@gmail.com>
2021-12-04 16:28:52 +0900

    complete chrome extension

--- chrome extension/index.js
+++ chrome extension/index.js
@@ -1,47 +1,32 @@
-const myLeads = [];
+let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const leadsStorage = JSON.parse(localStorage.getItem("myLeads"));
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-inputBtn.addEventListener("keydown", function (e) {
-  console.log("keydown?? e", e.key);
-  if (e.key === "Enter") {
-    if (
-      inputEl.value !== "" &&
-      inputEl.value !== null &&
-      inputEl.value !== undefined
-    ) {
-      myLeads.push(inputEl.value);
-    }
-
-    renderLeads();
-    // clear the input field
-    clearField();
-  }
-});
-
-inputBtn.addEventListener("click", function (e) {
-  if (
-    inputEl.value !== "" &&
-    inputEl.value !== null &&
-    inputEl.value !== undefined
-  ) {
-    myLeads.push(inputEl.value);
-  }
-
-  renderLeads();
-  // clear the input field
-  clearField();
-});
-// window.onkeydown = (e) => console.log(e);
-
-function clearField() {
-  document.getElementById("input-el").value = "https://";
+if (leadsStorage) {
+  myLeads = leadsStorage;
+  render(myLeads);
 }
 
-function renderLeads() {
+tabBtn.addEventListener("click", function () {
+  getCurrentTab();
+});
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  myLeads.push(tab.url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+}
+
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     // listItems +=
     //   "<li><a href='" +
     //   myLeads[i] +
@@ -50,7 +35,7 @@ function renderLeads() {
     //   "</a></li>";
 
     listItems += `<li>
-        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+        <a href='${leads[i]}' target='_blank'>${leads[i]}</a>
       </li>`;
 
     // a link 새탭에서 열기
@@ -65,4 +50,32 @@ function renderLeads() {
   }
   console.log("listItems", listItems);
   ulEl.innerHTML = listItems;
+}
+
+inputBtn.addEventListener("click", function (e) {
+  if (
+    inputEl.value !== "" &&
+    inputEl.value !== null &&
+    inputEl.value !== undefined
+  ) {
+    myLeads.push(inputEl.value);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)); //array to string
+    //JSON.parse()   //string to aray
+    render(myLeads);
+  }
+  // clear the input field
+  clearField();
+});
+
+deleteBtn.addEventListener("click", function () {
+  let result = confirm("Are you sure delete all?");
+  if (result) {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+  }
+});
+
+function clearField() {
+  document.getElementById("input-el").value = "https://";
 }
